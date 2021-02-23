d8277bb777e41e2f8a8129feac6a40e67684b252
Evgenii Ikonnikov <evgeniy3129@gmail.com>
2021-02-23 20:53:31 +0100

    Upvotes and homepage are ready

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class product(models.Model):
         return self.title
 
     def summary(self):
-        return body[:100]
+        return self.body[:100]
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y ')
